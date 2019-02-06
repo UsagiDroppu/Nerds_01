eb226caf5a96a5d3ad22e1b5f7e95c14e6c631fd
UsagiDroppu <UsagiDroppu@users.noreply.github.com>
2019-02-06 22:25:07 +0300

    кнопочки слайдера js

--- index.js
+++ index.js
@@ -2,13 +2,36 @@
 
 var slides = document.querySelectorAll('.js-presentation .presentation');
 var buttons = document.querySelectorAll(".inside-border");
+var jsbuttons = document.querySelectorAll(".presentation-buttons>li");
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
+
  
-function nextSlide() {
+ function nextSlide() {
+    goToSlide(currentSlide+1);
+}
+
+function goToSlide(n) {
     slides[currentSlide].className = 'presentation';
     buttons[currentSlide].className = 'inside-border';
-    currentSlide = (currentSlide+1)%slides.length;
+    currentSlide = (n+slides.length)%slides.length;
     slides[currentSlide].className = 'presentation showing';
     buttons[currentSlide].className = 'inside-border showing-button';
 }
+
+jsbuttons[0].onclick = function(){console.log("1");
+	goToSlide(0);
+	clearInterval(slideInterval);
+	slideInterval = setInterval(nextSlide,2000);
+}
+jsbuttons[1].onclick = function(){console.log("2");
+	goToSlide(1);
+	clearInterval(slideInterval);
+	slideInterval = setInterval(nextSlide,2000);
+}
+jsbuttons[2].onclick = function(){console.log("3");
+	goToSlide(2);
+	clearInterval(slideInterval);
+	slideInterval = setInterval(nextSlide,2000);
+}
+
