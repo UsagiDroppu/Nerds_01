3bc0b715a30a9b2feac9943a7afecdbc6800b95f
UsagiDroppu <UsagiDroppu@users.noreply.github.com>
2019-02-11 21:03:15 +0300

    кнопочки 1 2 3

--- catalog.js
+++ catalog.js
@@ -5,7 +5,8 @@ console.log("пек");
 var pictures = document.querySelectorAll(".cat-pictures>li");
 var price = document.querySelectorAll(".pictures-hover");
 var browser = document.querySelectorAll(".browser");
-var buttons = document.querySelectorAll("nav-buttons");
+var buttons = document.querySelectorAll(".nav-buttons");
+var currentButton = 0;
 
 for (var i = pictures.length - 1; i >= 0; i--) {
 	pictures[i].addEventListener("mouseover", picturesOn);
@@ -19,7 +20,7 @@ for (var i = buttons.length - 1; i >= 0; i--) {
 
 function picturesOn(){
 	for (var i = pictures.length - 1; i >= 0; i--) {
-		console.log(event.target);
+		
 		if (pictures[i] == this) {
 			price[i].className = 'pictures-hover-on';
 			browser[i].className = 'browser-on';
@@ -29,7 +30,7 @@ function picturesOn(){
 
 function picturesOff() {
 	for (var i = pictures.length - 1; i >= 0; i--) {
-		console.log(event.target);
+		
 		if (pictures[i] == this) {
 			price[i].className = 'pictures-hover';
 			browser[i].className = 'browser';
@@ -37,11 +38,28 @@ function picturesOff() {
 	}
 }
 
+
+
 function buttonsOn() {
-	for (var i = buttons.length - 1; i >= 0; i--) {
-		console.log(event.target);
-		if ((buttons[i] == this)&&(i!=(buttons.length-1)) {
-			buttons[i].className = '.nav-buttons--active';
+	for (var i = 0; i < buttons.length; i++) {
+		if (buttons[i].classList.contains('nav-buttons--active')){
+			currentButton=i;
+			console.log(currentButton);
+		};
+		if (!(this.classList.contains('nav-buttons-next'))) {
+			// console.log(event.target);
+			if (buttons[i]==this) {
+				buttons[i].className = 'nav-buttons nav-buttons--active';	
+			}
+			else {
+				if (!(buttons[i].classList.contains('nav-buttons-next'))){
+					buttons[i].className = 'nav-buttons';
+				}
+			}
 		}
+	}
+	if (this.classList.contains('nav-buttons-next') && !buttons[currentButton+1].classList.contains('nav-buttons-next')){
+			buttons[currentButton].className = 'nav-buttons';
+			buttons[currentButton+1].className = 'nav-buttons nav-buttons--active';
 	}
 }
